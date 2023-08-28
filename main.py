30d454522bf1c332b8b0af473e593b530c0c0f3d
TatzUwUki <leonlukassalzmann@gmail.com>
2023-08-28 12:40:06 +0200

    test2

--- main.py
+++ main.py
@@ -8,7 +8,8 @@
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    pass
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 print("hi")
+print("wowzers")
