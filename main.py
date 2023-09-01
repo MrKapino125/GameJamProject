6d7b0055381015859a0c167fb5e66667a2530a1e
MrKapino <117902509+MrKapino@users.noreply.github.com>
2023-09-01 09:28:35 +0200

    Reset

--- main.py
+++ main.py
@@ -12,24 +12,43 @@ height = 900
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("CardGame")
 clock = pygame.time.Clock()
-
 eventHandler = eventhandler.Eventhandler()
-states = [state.MenuState(screen, eventHandler), state.GameState(screen, eventHandler), state.EndState(screen, eventHandler)]
-currentState = states[0]
 background = pygame.image.load("res/gdbackground4.png")
+
+states = None
+currentState = None
+
+
+def init():
+    global states
+    global currentState
+
+    states = [state.MenuState(screen, eventHandler), state.GameState(screen, eventHandler),
+              state.EndState(screen, eventHandler)]
+    currentState = states[0]
+
 
 def tick():
     global currentState
 
     currentState = currentState.tick(states)
     eventHandler.tick()
-
-
 def render():
     screen.blit(background, (0, 0))
     currentState.render()
 
+
+init()
 while True:
+    if eventHandler.is_pressed["r"] and not eventHandler.is_lockedp["r"]:
+        eventHandler.is_lockedp["r"] = True
+        for _ in range(3):
+            del states[0]
+        currentState = None
+        init()
+    if not eventHandler.is_pressed["r"]:
+        eventHandler.is_lockedp["r"] = False
+
     tick()
     render()
 
